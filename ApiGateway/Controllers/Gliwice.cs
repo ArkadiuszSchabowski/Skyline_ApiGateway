18cacb325a399523ab319c1373ed7c81f42980a4
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-09 06:54:59 +0100

    feat: create API Gateway Web API project with default configuration

---
+++ ApiGateway/Controllers/Gliwice.cs
@@ -0,0 +1,31 @@
+﻿using ApiGateway.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Gliwice : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<MovieDto> DummyMoviesGliwice()
+        {
+            var movies = new List<MovieDto> {
+                new MovieDto {Id = 1, Title = "Gladiator"},
+                new MovieDto {Id = 2, Title = "Shrek"},
+                new MovieDto {Id = 3, Title = "Wilk z Wall Street"},
+                new MovieDto {Id = 4, Title = "Incepcja"},
+                new MovieDto {Id = 5, Title = "Matrix"},
+                new MovieDto {Id = 6, Title = "Król Lew"},
+                new MovieDto {Id = 7, Title = "Forrest Gump"},
+                new MovieDto {Id = 8, Title = "Ojciec chrzestny"},
+                new MovieDto {Id = 9, Title = "Skazani na Shawshank"},
+                new MovieDto {Id = 10, Title = "Władca Pierścieni: Drużyna Pierścienia"}
+            };
+            return Ok(movies);
+        }
+    }
+
+}
+
---
+++ ApiGateway/Controllers/Katowice.cs
@@ -0,0 +1,29 @@
+﻿using ApiGateway.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Katowice : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<MovieDto> DummyMoviesKatowice()
+        {
+            var movies = new List<MovieDto>{
+                new MovieDto {Id = 1, Title = "Avatar"},
+                new MovieDto {Id = 2, Title = "Gwiezdne Wojny: Nowa nadzieja"},
+                new MovieDto {Id = 3, Title = "Piraci z Karaibów: Klątwa Czarnej Perły"},
+                new MovieDto {Id = 4, Title = "Hobbit: Niezwykła podróż"},
+                new MovieDto {Id = 5, Title = "Czarna Pantera"},
+                new MovieDto {Id = 6, Title = "Joker"},
+                new MovieDto {Id = 7, Title = "Titanic"},
+                new MovieDto {Id = 8, Title = "Łowca androidów"},
+                new MovieDto {Id = 9, Title = "Toy Story"},
+                new MovieDto {Id = 10, Title = "Mad Max: Na drodze gniewu"}
+            };
+            return Ok(movies);
+        }
+    }
+}
